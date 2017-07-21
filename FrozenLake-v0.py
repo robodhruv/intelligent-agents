6e1f57731238fef988696b6ec875824b9dd80ff1
ishan <ishantarunesh@gmail.com>
2017-07-21 18:45:53 +0530

    Display Merged, Arbitrary Board

--- FrozenLake-v0.py
+++ FrozenLake-v0.py
@@ -20,49 +20,121 @@ The observation space is 16 cells on the board
 
 Positions are given index = 4*row + col where row,col belongs to {0,1,2,3}
 '''
+# Colors
+RED = (255,51,51)
+GREEN = (102,255,102)
+BLUE = (0,0,255)
+WHITE = (224,224,224)
+BLACK = (64,64,64)
+
 
 import numpy as np
+import pygame
 import matplotlib.pyplot as plt
 from random import randint
+pygame.init()
 
-observation_space = 16
+def initializeVariable():
+	global observation_space, startPoint , endPoint, holes
+	print "Enter number of rows:"
+	rows = int(raw_input(">"))
+	print "Enter number of cols:"
+	cols = int(raw_input(">"))
+	observation_space = [rows,cols]
+	print "Input Board:"
+	i = 0
+	while(i < rows):
+		r1 = raw_input()
+		j = 0
+		while(j < cols):
+			if(r1[j] == "S"):
+				startPoint = [j , i]
+			elif(r1[j] == "H"):
+				holes.append([j,i])
+			elif(r1[j] == "G"):
+				endPoint = [j,i]
+			j += 1
+		i += 1
+	return
+
+observation_space = []
+startPoint = []
+endPoint = []
+holes = []
 action_space = 4
 alpha = 0.1
-epsilon = 0.5 # Ratio of Exploitation vs Exploration
-episodes = 100
+epsilon = 0.4 # Ratio of Exploitation vs Exploration
+episodes = 10000
 
-qTable = np.zeros((observation_space,action_space),np.float32)
+
+initializeVariable()
+
+move_size = 40
+size = [20+move_size*(observation_space[1]),20+move_size*(observation_space[0])]
+screen = pygame.display.set_mode(size)
+def drawBoard():
+	global startPoint , endPoint , holes
+	pygame.display.set_caption("Frozen Lake")
+	pygame.draw.rect(screen,WHITE, [10,10,320,320],10)
+	for i in range(observation_space[0]):
+		for j in range(observation_space[1]):
+			if([j,i] in holes):
+				pygame.draw.rect(screen,RED,[10+move_size*j,10+move_size*i,move_size,move_size])
+			elif ([j,i] == startPoint):
+				pygame.draw.rect(screen,BLUE,[10+move_size*j,10+move_size*i,move_size,move_size])
+			elif ([j,i] == endPoint):
+				pygame.draw.rect(screen,GREEN,[10+move_size*j,10+move_size*i,move_size,move_size])
+			else:
+				pygame.draw.rect(screen,WHITE,[10+move_size*j,10+move_size*i,move_size,move_size])
+	return
+
+qTable = np.zeros((observation_space[0]*observation_space[1],action_space),np.float32)
 count_exploration = 0
 count_exploitation = 0
 
 def step(state_i,move):
-	x_pos = state_i%4
-	y_pos = state_i/4
+	global observation_space ,endPoint , startPoint, holes
+	x_pos = state_i%(observation_space[1])
+	y_pos = state_i/(observation_space[1])
 	reward = 0
 	done = False
-	if(move == 0):
-		x_pos = min(x_pos+1,3)
-	elif (move == 2):
+	if(move == 0): #Right
+		x_pos = min(x_pos+1,observation_space[1]-1)
+	elif (move == 2): #Left
 		x_pos = max(0,x_pos-1)
-	elif (move == 1):
+	elif (move == 1): #Up
 		y_pos = max(0,y_pos-1)
-	elif (move == 3):
-		y_pos = min(y_pos+1,3)
+	elif (move == 3): #Down
+		y_pos = min(y_pos+1,observation_space[0]-1)
 
-	if(x_pos == 3 and y_pos == 3):
+	if(x_pos == endPoint[0] and y_pos == endPoint[1]):
 		reward = 1
 		done = True
-	elif ((x_pos == 3 and y_pos == 1) or (x_pos == 3 and y_pos == 2) or (x_pos == 1 and y_pos == 2)):
+	elif ([x_pos , y_pos] in holes):
 		reward = -1
 		done = True
-	return (4*y_pos+x_pos,reward,done)
+	return (observation_space[1]*y_pos+x_pos,reward,done)
+
+def generateInitialPos():
+	global startPoint, endPoint, holes,observation_space
+	rows = observation_space[0]
+	cols = observation_space[1]
+	fate = randint(1,100)
+	if(fate < 50):
+		return rows*startPoint[0] + startPoint[1]
+	else:
+		randomPos = randint(0,rows*cols-1)
+		if(([randomPos%cols,randomPos/cols] in holes) or ([randomPos%cols,randomPos/cols] == endPoint)):
+			return generateInitialPos()
+		else:
+			return randomPos
 
 def main():
 	global qTable, count_exploitation , count_exploration
 	for i in range(episodes):
-		if (i % 100 == 0):
-			print (i/100) , "%"
-		prev_state = 0
+		if((i+1) % 100 == 0):
+			print (i+1)*100/episodes, "%"
+		prev_state =  generateInitialPos()
 		done = False
 		while(not done):
 			fate = randint(1,100)
@@ -83,27 +155,34 @@ def main():
 				state_n, reward_n , done = step(prev_state, action)
 				prev_state = state_n
 
-def display(table):
-	print "-----------------------------------------------------"
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[0][1],table[1][1],table[2][1],table[3][1])
-	print "|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|" %(table[0][2],table[0][0],table[1][2],table[1][0],table[2][2],table[2][0],table[3][2],table[3][0])
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[0][3],table[1][3],table[2][3],table[3][3])
-	print "-----------------------------------------------------" 
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[4][1],table[5][1],table[6][1],table[7][1])
-	print "|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|" %(table[4][2],table[4][0],table[5][2],table[5][0],table[6][2],table[6][0],table[7][2],table[7][0])
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[4][3],table[5][3],table[6][3],table[7][3])
-	print "-----------------------------------------------------"
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[8][1],table[9][1],table[10][1],table[11][1])
-	print "|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|" %(table[8][2],table[8][0],table[9][2],table[9][0],table[10][2],table[10][0],table[11][2],table[11][0])
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[8][3],table[9][3],table[10][3],table[11][3])
-	print "-----------------------------------------------------"
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[12][1],table[13][1],table[14][1],table[15][1])
-	print "|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|%.2f    %.2f|" %(table[12][2],table[12][0],table[13][2],table[13][0],table[14][2],table[14][0],table[15][2],table[15][0])
-	print "|    %.2f    |    %.2f    |    %.2f    |    %.2f    |" %(table[12][3],table[13][3],table[14][3],table[15][3])
-	print "-----------------------------------------------------"
-
+def showAgent(n):
+	global qTable, startPoint
+	for i in range(n):
+		prev_state = startPoint[0]+observation_space[1]*startPoint[1]
+		done = False
+		screen.fill((0,0,0))
+		drawBoard()
+		pygame.draw.circle(screen, (100,200,150) , [10+ move_size/2+move_size*(prev_state%(observation_space[1])) , 10+ move_size/2+move_size*(prev_state/(observation_space[1]))],15)
+		pygame.display.flip()
+		pygame.time.wait(1000)
+		while(not done):
+			print [(prev_state%(observation_space[1])) , (prev_state/(observation_space[1]))]
+			max_state = max(qTable[prev_state,:])
+			max_action_list = []
+			for i,x in enumerate(qTable[prev_state,:]):
+				if(x == max_state):
+					max_action_list.append(i)
+			max_action = max_action_list[randint(0,len(max_action_list)-1)]
+			state_n , reward_n, done = step(prev_state,max_action)
+			screen.fill((0,0,0))
+			drawBoard()
+			pygame.draw.circle(screen, (100,200,150) , [10+ move_size/2+move_size*(state_n%(observation_space[1])) , 10+ move_size/2+move_size*(state_n/(observation_space[1]))],15)
+			pygame.display.flip()
+			prev_state = state_n
+			pygame.time.wait(1000)
 
 if __name__ == '__main__':
 	main()
-	display(qTable)
+	print qTable
 	print "Exploitation : %r , Exploration : %r " %(count_exploitation,count_exploration)
+	showAgent(2)
